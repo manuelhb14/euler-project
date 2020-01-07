3399a04d0652908e2d5969d6d046fbcea8c473ef
manuelhb14 <mhernandezb96@gmail.com>
2020-01-07 01:33:56 -0600

    solved problem 5. All numbre must be divisble by the last digit so range can a mutliplier of last number

---
+++ smallestMultiple/solution.py
@@ -0,0 +1,14 @@
+def smallest_mult (x,y):
+    start = y
+    divisible = False
+    while not divisible:
+        i = x
+        while start%i==0 and i<y:
+            i+=1
+            divisible = True
+        if start%i!=0:
+            start+=10
+            divisible = False
+    return start
+
+print(smallest_mult(1,20))
